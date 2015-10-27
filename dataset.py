9201586ceed23014153ecac4de3d776848e25d74
Yash Chandak <yashchandak@yahoo.in>
2015-10-27 17:47:25 +0530

    dataset added

--- dataset.py
+++ dataset.py
@@ -5,15 +5,23 @@ Created on Tue Oct 27 11:41:03 2015
 @author: yash and ankit
 """
 import numpy as np
-import cv2 as cv
+import os, os.path
+import cv2
 
 inp = 4
 output = 1
-data = [[0,0,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[0,1,1,1,1],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,0,1,1,1],[1,1,0,0,0],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
+
+dim = 20
+valid_images = [".jpg",".gif",".png",".tga"]
+data = []
+test = []
+
+#data2 = [[0,0,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[0,1,1,1,1],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,0,1,1,1],[1,1,0,0,0],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
 
 def generate_data():
-
-    #data = [[0,0,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[0,1,1,1,1],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,0,1,1,1],[1,1,0,0,0],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
+    global data, test
+    data = [[0,0,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[0,1,1,1,1],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,0,1,1,1],[1,1,0,0,0],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
+    test = data    
     #data = [[.1,.1,0.75],[.4,.1,0.75],[.1,.4,0.75],[.1,.7,0],[.1,.9,0], [.3,.8,0], [.7,.7,0.25],[.6,.99,0.25],[.9,.7,0.25],[.8,.1,1],[.7,.3,1],[.99,.2,1],[.5,.5,0.5],[.8,.5,0.5], [.4,.7,0.5], [.6,.8,0.5]]
     #data = [[.1,.1,0,0],[.4,.1,0,0],[.1,.4,0,0],[.1,.7,0,1],[.1,.9,0,1], [.3,.8,0,1], [.7,.7,1,0],[.6,.99,1,0],[.9,.7,1,0],[.8,.1,1,1],[.7,.3,1,1],[.99,.2,1,1]]
     #data = [[0,0,0],[0,1,1],[1,0,1],[1,1,0]] #XOR   
@@ -21,11 +29,52 @@ def generate_data():
     #data = np.array([[0,0],[1,1],[4,2],[5,4],[6,6],[7,7],[8,9],[9,9],[12,10],[13,9],[15,8],[16,7],[20,4],[21,2],[23,0],[24,0]], dtype = 'float')
     #data = np.array([[.1,.2,.3,.1],[.3,.5,.8,.2]])    #manual test data
     
-    return data
+    #return data
 
 def get_data(index):
     #in1, in2, out1 = data[index]             #parse the data to get input and expected output
     #in1, out1 = data[index]    
     in1, in2, in3, in4, out1 = data[index]
     return np.array([in1,in2, in3, in4]),np.array([out1])
+    #return data[index][0], data[index][1]
 
+def get_test(index):
+    in1, in2, in3, in4, out1 = test[index]
+    return np.array([in1,in2, in3, in4]),np.array([out1])
+    #return test[index][0], test[index][1]    
+    
+def read_from_folder(path, val):
+    imgs = []
+    for f in os.listdir(path):
+        ext = os.path.splitext(f)[1]
+        if ext.lower() not in valid_images:
+            continue
+        filename = path+'/'+f
+        img = cv2.imread(filename,0)
+        img = cv2.resize(img, (dim, dim))
+        img = img.flatten()
+        img = img/255.0
+        imgs.append([img, val])
+    return imgs
+    
+def compile_data():
+    global data,test
+    
+    imgs = read_from_folder("D:/ToDo/datasets/101_ObjectCategories/ant", np.array([0,1]))
+    count = int(0.9*len(imgs))
+    
+    data.extend(imgs[:count])
+    test.extend(imgs[count:])
+
+    imgs = read_from_folder("D:/ToDo/datasets/101_ObjectCategories/umbrella", np.array([1,0]))
+    count = int(0.9*len(imgs))
+    
+    data.extend(imgs[:count])
+    test.extend(imgs[count:])
+    
+    np.random.shuffle(data)
+    np.random.shuffle(test)
+    print 'Datset made successfully'
+
+#compile_data()
+generate_data()
--- mulitlayerBNN.py
+++ mulitlayerBNN.py
@@ -15,9 +15,9 @@ Multilayer backpropagation neural network
 
 TODO:
 1) convert to modular class/object based design
-2) addition of biases
-3) generalise to n number of hidden layers
-4) optimisation
+2) *addition of biases
+3) [DONE] generalise to n number of hidden layers
+4) *optimisation
         a) momentum
         b) conjugated gradient descent        
 5) normalise input and output data
@@ -25,7 +25,9 @@ TODO:
 7) [DONE] cache constant intermediate results instead of recalculating
 8) end training based on difference from prv error
 9) [DONE] Matrix notation for weight updates
-10)intermediate storage of weights in some file (for recovery/comparison)
+10)*intermediate storage of weights in some file (for recovery/comparison)
+11) geometric image manip
+12)*GPU usage
 
 @author: yash and ankit
 """
@@ -35,25 +37,8 @@ from matplotlib import cm
 import matplotlib.pyplot as plt
 import numpy as np
 import time
-#import dataset
+import dataset
 
-def generate_data():
-
-    data = [[0,0,0,0,0],[0,0,0,1,1],[0,0,1,0,1],[0,0,1,1,0],[0,1,0,0,1],[0,1,0,1,0],[0,1,1,0,0],[0,1,1,1,1],[1,0,0,0,1],[1,0,0,1,0],[1,0,1,0,0],[1,0,1,1,1],[1,1,0,0,0],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
-    #data = [[.1,.1,0.75],[.4,.1,0.75],[.1,.4,0.75],[.1,.7,0],[.1,.9,0], [.3,.8,0], [.7,.7,0.25],[.6,.99,0.25],[.9,.7,0.25],[.8,.1,1],[.7,.3,1],[.99,.2,1],[.5,.5,0.5],[.8,.5,0.5], [.4,.7,0.5], [.6,.8,0.5]]
-    #data = [[.1,.1,0,0],[.4,.1,0,0],[.1,.4,0,0],[.1,.7,0,1],[.1,.9,0,1], [.3,.8,0,1], [.7,.7,1,0],[.6,.99,1,0],[.9,.7,1,0],[.8,.1,1,1],[.7,.3,1,1],[.99,.2,1,1]]
-    #data = [[0,0,0],[0,1,1],[1,0,1],[1,1,0]] #XOR   
-    #data = [[0,0,0],[0,1,1],[1,0,1],[1,1,0]] #AND
-    #data = np.array([[0,0],[1,1],[4,2],[5,4],[6,6],[7,7],[8,9],[9,9],[12,10],[13,9],[15,8],[16,7],[20,4],[21,2],[23,0],[24,0]], dtype = 'float')
-    #data = np.array([[.1,.2,.3,.1],[.3,.5,.8,.2]])    #manual test data
-    
-    return data
-
-def get_data(index):
-    #in1, in2, out1 = data[index]             #parse the data to get input and expected output
-    #in1, out1 = data[index]    
-    in1, in2, in3, in4, out1 = data[index]
-    return np.array([in1,in2, in3, in4]),np.array([out1])
 
 
 def activate(z, derivative = False):
@@ -80,9 +65,10 @@ def train_nets():
     for epoch in xrange(iter_no):
         #update based on each data point
         error_sum = 0
+        test_error_sum = 0
         for i in xrange(len(data)):            
             
-            inputs, expected = get_data(i)
+            inputs, expected = dataset.get_data(i)
             execute_net(inputs)
             error = expected - receptors[depth-1]   #error vector corresponding to each output
             error_sum += sum(abs(error))
@@ -95,10 +81,18 @@ def train_nets():
             for index in xrange(depth-1, 0, -1):
                 synapses[index] += learning_rate*deltas[index].reshape(topology[index+1],1)*receptors[index-1]
             synapses[0] += learning_rate*deltas[0].reshape(topology[1],1)*inputs
-           
+         
+         
+        for i in xrange(len(test)):
+            inputs, expected = dataset.get_test(i)
+            execute_net(inputs)
+            test_error_sum += sum(abs(expected - receptors[depth-1]))
+        
         err[epoch] = error_sum/len(data)
-        if epoch%1000 == 0:
-            print "Iteration no: ", epoch, "    error: ", err[epoch]
+        test_err[epoch] = test_error_sum/len(test)
+        
+        if epoch%100 == 0:
+            print "Iteration no: ", epoch, "    error: ", err[epoch], " test error: ", test_err[epoch]
 
     
 def execute_net(inputs):
@@ -113,8 +107,8 @@ def execute_net(inputs):
 NETWORK TOPOLOGY
 """
 e = 2.718281828
-inp = 4                 #input vector dimensions:
-nodes_output  = 1  #number of outputs
+inp = dataset.inp                 #input vector dimensions:
+nodes_output  = dataset.output  #number of outputs
 learning_rate = 0.5
 momentum = 0.3
 iter_no = 25000              #training iterations
@@ -123,10 +117,12 @@ iter_no = 25000              #training iterations
 """
 DATA generation, internediate values and weights initialisation
 """
-data = generate_data()                                      #get data
+data = dataset.data                                      #get data
+test = dataset.test
 err = np.zeros(iter_no)
+test_err = np.zeros(iter_no)
 
-topology = np.array([inp,20,8,nodes_output])
+topology = np.array([inp,4,8,4,nodes_output])
 depth = topology.size - 1
 
 synapses = [np.random.random((size2,size1)) for size1,size2 in zip(topology[0:depth],topology[1:depth+1])]
@@ -137,7 +133,7 @@ deltas = [np.zeros(size, 'float') for size in topology[1:]]
 def main():
     train_nets()
     plotit(range(iter_no), err, 1, 'iteration number', 'error value', 'Error PLot')
-    
+    plotit(range(iter_no), test_err, 1, 'iteration number', 'error value', 'Error PLot')
 #    while(1):
 #        v,w,x,y = input()
 #        if v > 99:
