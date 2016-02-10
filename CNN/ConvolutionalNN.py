8082597aa559be0f34a81a859a5de8b9171ac50a
Yash Chandak <yashchandak@yahoo.in>
2016-02-10 11:20:12 +0530

    checkpoint: done coding, but few bugs

--- CNN/ConvolutionalNN.py
+++ CNN/ConvolutionalNN.py
@@ -5,24 +5,27 @@ Created on Thu Feb 04 17:17:10 2016
 @author: yash
 
 ToDo:
-[1] :   Discard previous activations of layers during test time (on pi)
-http://cs231n.github.io/convolutional-networks/
-
-[2] :   Check activaiton of 2d matrices
+[1] :   Discard previous activations of layers during test time (on pi) http://cs231n.github.io/convolutional-networks/
+[2] : Check activaiton of 2d matrices
 [3] : ndimage.convolve flips the filter, shouldn't be a problem though as it is flipped throughout the program
-[4] : First filter works only on 2d image right now
+[4] : connect filters to all previous filter outputs, not just the one in it's axis
+[5] : conv fitler error not required to be stored for all conv layers. One is sufficient
+[6] : batch updates
+[7] : droupouts
+[8] : momentum for filter weights
+[9] : First bias problem for FC NNet
+[10]: Effecient way to address array? a[1][1] or a[1,1]
 
 correctness of hyper parameters :
 [(layer1 - filter size + 2*padding)/stride] + 1  =  (an integer)
 
 """
 
-import scipy as sp
+import scipy.ndimage as sp
 import numpy as np
 import time
 import activation as act
 import plotting as plot
-import features
 import dataset
 
 """------------------------ General Variables ----------------------------"""
@@ -38,29 +41,33 @@ err         = np.zeros(iter_no)         #keep track of sample error after each i
 test_err    = np.zeros(iter_no)         #keep track of test error after each iteration
 
 """----------------------- Variables for Conv NNet -----------------------"""
-filter_size = 5
-filter_count= 5
+learn_rate_conv = 0.0001
 
-conv_layers = 3
+filter_size = 5
+step        = filter_size//2
+filter_count= 3
+
+conv_layers = 1
 stride      = 1
 pool_size   = 2
 
 
 #filters = convlayer no., filter no., 3d filter dimension (connecting all previous filters)
-filters     = np.random.randn(conv_layers, filter_count, filter_count, filter_size, filter_size )  
-convolved   = [np.zeros(filter_count, inp/pool_size**i, inp/pool_size**i) for i in range(conv_layers)]
-conv_delta  = [np.zeros(filter_count, inp/pool_size**i, inp/pool_size**i) for i in range(conv_layers)]
-switches    = [np.zeros(filter_count, inp/pool_size**i, inp/pool_size**i) for i in range(1, conv_layers+1)]
-pooled      = [np.zeros(filter_count, inp/pool_size**i, inp/pool_size**i) for i in range(1, conv_layers+1)]
-
-conv_bias   = np.zeros(conv_layers, filter_count)
+filters     = np.random.randn(conv_layers, filter_count, filter_size, filter_size )#/filter_size**2  
+convolved   = [np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i)) for i in range(conv_layers)]
+conv_delta  = np.array([np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i)) for i in range(conv_layers)])
+switches    = [np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i, 2)) for i in range(1, conv_layers+1)]
+pooled      = [np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i)) for i in range(1, conv_layers+1)]
+conv_error  = [np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i)) for i in range(1, conv_layers+1)]
+conv_bias   = np.zeros((conv_layers, filter_count))
  
 """---------------------- Variables for FC NNet -------------------------"""
 
-learning_rate= 0.3
-momentum    = 0.3
+learning_rate= 0.5
+momentum    = 0.5
 
-topology    = np.array([inp/(pool_size**(conv_layers-1)),1024,nodes_output])
+inp_vector  = filter_count*(inp//(pool_size**(conv_layers)))**2
+topology    = np.array([inp_vector,1024,nodes_output])
 depth       = topology.size - 1
 
 synapses    = [np.random.randn(size2,size1) for size1,size2 in zip(topology[0:depth],topology[1:depth+1])]
@@ -70,10 +77,10 @@ bias        = [np.zeros(size, 'float') for size in topology[:]]
 receptors   = [np.zeros(size, 'float') for size in topology[:]] #does not have inputs
 deltas      = [np.zeros(size, 'float') for size in topology[:]]
 
-
+"""------------------------------------------------------------------------"""
     
 def train_nets():
-    global  err, test_err, deltas, synapses, prv_update, curr_update
+    global  err, test_err, deltas, synapses, prv_update, curr_update, filters, conv_delta, conv_error, conv_bias 
     
     error = 0    
     for epoch in xrange(iter_no):        
@@ -83,8 +90,8 @@ def train_nets():
         test_error_sum = 0
         
         for i in xrange(len(data)):            
-            receptors[0], expected = dataset.get_data(i)
-            execute_net(receptors[0])
+            inputs, expected = dataset.get_data(i)
+            execute_net(inputs)
             
             error = expected - receptors[depth]   #error vector corresponding to each output
             error_sum += sum(abs(error))
@@ -94,10 +101,11 @@ def train_nets():
             #compute deltas for FC NNet receptors
             deltas[depth] = act.activate(receptors[depth],True)*error
             for index in xrange(depth-1, -1, -1):
-                if index == 0:
-                    deltas[index] = act.activate(receptors[index],True, fn='ReLu')*synapses[index].transpose().dot(deltas[index+1])
-                else:                  
-                    deltas[index] = act.activate(receptors[index],True)*synapses[index].transpose().dot(deltas[index+1])
+                
+                fn = 'Sigmoid'
+                if index == 0: #at index 0 is the ReLu output of Conv NNet
+                    fn = 'ReLu'  
+                deltas[index] = act.activate(receptors[index],True, fn)*synapses[index].transpose().dot(deltas[index+1])
             
             #update the weights of FC NNet synapses
             for index in range(depth-1, -1, -1):
@@ -108,23 +116,43 @@ def train_nets():
             prv_update = curr_update
         
             #compute deltas for conv NNet layers
-            conv_error = deltas[0].reshape(pooled[conv_layers-1].shape) #reshape the error from FC NNet for conv NNet
-            conv_delta.fill(0)                                          #flush all previous delta values
-            
+            conv_error[conv_layers - 1] = deltas[0].reshape(pooled[conv_layers-1].shape) #reshape the error from FC NNet for conv NNet
+            #conv_delta.fill(0)  #flush all previous delta values
+            conv_delta  = [np.zeros((filter_count, inp//pool_size**i, inp//pool_size**i)) for i in range(conv_layers)]
             for conv in range(conv_layers - 1, -1, -1):
                 for fil in range(filter_count):
                     #upsampling the error matrix using switches of max pool
                     switch = switches[conv][fil]
-                    for i in switch.shape[0]:
-                        for j in switch.shape[1]:
-                            conv_delta[conv][fil][switch[i][j][0]][switch[i][j][1]] = conv_error[fil][i][j]
+                    for i in range(switch.shape[0]):
+                        for j in range(switch.shape[1]):
+                            conv_delta[conv][fil][switch[i][j][0]][switch[i][j][1]] = conv_error[conv][fil][i][j]
         
-                    #can be done outside the filter loop
+                    #compute the delta for current layer's filter #[can be done outside the filter loop]
+                    """it's getting multiplied twice by the the activation as receptor[0] = convolved[last layer] """
                     conv_delta[conv][fil] = conv_delta[conv][fil]*act.activate(convolved[conv][fil], True ,fn = 'ReLu')
                     
+                    #compute the error for previous layer
+                    """no need for conv_error, pass error to upscale function and store in conv_delta directly"""
+                    if conv>0:
+                        conv_error[conv-1][fil] = sp.convolve(conv_delta[conv][fil],
+                                                                  filters[conv][fil].transpose(),
+                                                                  mode = 'constant' )
         
-        
-        
+                    w,h = pooled[conv-1][fil].shape
+                    #update the weights of the filter 
+                    prv = pooled[conv-1][fil]
+                    if conv == 0:
+                        prv = inputs                    
+                    for i in range(filter_size):
+                        for j in range(filter_size):
+                            #print w,h,i,j,step
+                            #print max(0, i-step),min(w, w+i-step), max(0, j-step), min(h, h+j-step), max(0, step-i),min(w, w+step-i),max(0, step-j),min(h, h+step-j)
+                            """it's convolution only!! but of 'valid' type in scipy.signal"""                            
+                            filters[conv][fil][i][j] += learn_rate_conv*sum(prv[max(0, i-step):min(w, w+i-step),max(0, j-step):min(h, h+j-step)]
+                                                                            *conv_delta[conv][fil][max(0, step-i):min(w, w+step-i),max(0, step-j):min(h, h+step-j)])
+                    conv_bias[conv][fil] += learn_rate_conv*sum(conv_delta[conv][fil])
+                    
+        #compute the validation set error        
         for i in xrange(len(test)):
             inputs, expected = dataset.get_test(i)
             execute_net(inputs)
@@ -134,7 +162,6 @@ def train_nets():
             tt[pos] = 1            
                 
             test_error_sum += sum(abs(expected - tt))
-            #test_error_sum += sum(abs(expected - receptors[depth-1]))
         
         err[epoch] = error_sum/len(data)
         test_err[epoch] = test_error_sum/(2*len(test)) #single misclassification creates an error sum of 2.
@@ -150,18 +177,16 @@ def execute_net(inputs):
     for conv in range(conv_layers):        
         for fil in range(filter_count):            
             #convolve and activate
-            if conv == 0: 
-                #for first convolution do it on the input image
-                convolved[conv][fil] = act.activate(sp.ndimage.convolve(inputs, filters[0][fil][0], mode = 'Constant') + conv_bias[conv][fil], derivative = 'False', fn = 'ReLu')
-            else:
-                #do remaining colvolutions on the previous pooled matrix
-                convolved[conv][fil] = act.activate(sp.ndimage.convolve(pooled[conv-1], filters[conv][fil], type = 'Constant') + conv_bias[conv][fil], derivative = 'False', fn = 'ReLu')
+            cur = inputs #for first convolution do it on the input image
+            if conv > 0: 
+                cur = pooled[conv-1][fil]
+            convolved[conv][fil] = act.activate(sp.convolve(cur, filters[conv][fil], mode = 'constant') + conv_bias[conv][fil], False, 'ReLu')
             
             #downsampling using max pooling
             for x in range(0,convolved[conv][fil].shape[0], pool_size):
                 for y in range(0, convolved[conv][fil].shape[1], pool_size):                    
                     maximum = -999999
-                    pos = (x,y)
+                    pos = [x,y]
                     
                     #selecting the max from the pooling window
                     for i in range(pool_size):
@@ -169,23 +194,24 @@ def execute_net(inputs):
                             
                             if convolved[conv][fil][x+i][y+j] > maximum:
                                 maximum = convolved[conv][fil][x+i][y+j]
-                                pos = (x+i, y+j)
+                                pos = [x+i, y+j]
        
                     pooled[conv][fil][x/pool_size][y/pool_size] = maximum
-                    switches[conv][fil][x/pool_size][y/pool_size] = pos
+                    switches[conv][fil][x/pool_size][y/pool_size][0] = pos[0]
+                    switches[conv][fil][x/pool_size][y/pool_size][1] = pos[1]
 
     #pass the CNN output to FC NNet
     receptors[0] = pooled[conv_layers-1].reshape(pooled[conv_layers-1].size)
         
     for index in xrange(0,depth): 
-        #activate the nodes based on sum of incoming synapses
+        #activate the nodes based on sum of incoming synapses and bias
         if index == 0:
              receptors[index+1] = act.activate(synapses[index].dot(receptors[index]))# remove bias for 0th
         else:
              receptors[index+1] = act.activate(synapses[index].dot(receptors[index]) + bias[index+1]) 
 
 def predict(img):    
-    execute_net(features.get_HoG(img))
+    execute_net(dataset.preprocess(img))
     print receptors[depth]
     pos = np.argmax(receptors[depth])
     print dataset.folders[pos]
--- CNN/activation.py
+++ CNN/activation.py
@@ -14,19 +14,29 @@ import numpy as np
 e = 2.718281828
 
 def activate(z, derivative = False, fn = 'Sigmoid' ):
-    #Sigmoidal activation function  
+    
+    #Sigmoidal activation function
     if fn == 'Sigmoid':
        
         if derivative:
-            return z*(1-z)        
-        return 1/(1+e**-z)
+            return z*(1-z)   
+        else:
+            return 1/(1+e**-z)
     
     #Relu activation function    
     elif fn == 'ReLu':
-        if derivative:
-            return np.array([1 if item>0 else 0.01 for item in z])
-        else:
-            return np.array([max(0.01, item) for item in z])
+        if len(z.shape) == 1:
+            if derivative:
+                return np.array([1 if item>0.01 else 0.01 for item in z])
+            else:
+                return np.array([max(0.01, item) for item in z])
+        
+        elif len(z.shape) == 2:
+            w,h = z.shape
+            if derivative:
+                return np.array([[1 if item>0.01 else 0.01 for item in z[i]] for i in range(h)])
+            else:
+                return np.array([[max(0.01, item) for item in z[i]] for i in range(h)])            
             
     #tanh activation function
     elif fn == 'Tanh':
@@ -34,3 +44,6 @@ def activate(z, derivative = False, fn = 'Sigmoid' ):
             return 1-(z**2)
         else:
             return (1-e**(-2*z))/(1+e**(-2*z))
+            
+    else:
+        print 'ERROR! invalid function!'
